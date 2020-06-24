972b85730cb054f1b32fd1e031c9d37d13ce6fee
peppato <cfcardillo23@gmail.com>
2020-06-24 07:52:24 -0400

    adding git lexicon

--- R/data.R
+++ R/data.R
@@ -12,3 +12,18 @@
 #' }
 #' @source made by scraping AWS CLI help docs
 "lexicon_aws"
+
+#' A giftwrap-formatted lexicon of all git commands
+#'
+#' A dataset containing all base, commands, and subcommands from git
+#'
+#' @format A dataset containing all base, commands, and subcommands from git
+#' \describe{
+#'   \item{base}{the base, always git}
+#'   \item{command}{the command, such as init, pull, or status}
+#'   \item{subcommand}{NA for git lexicon}
+#'   \item{giftwrap_command}{the base, command, and subcommand together}
+#'   ...
+#' }
+#' @source made by scraping git help docs
+"lexicon_git"
