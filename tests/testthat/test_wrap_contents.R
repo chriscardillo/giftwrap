4e8069445b28a809bf555dc30179b993cfc2c0ab
peppato <cfcardillo23@gmail.com>
2020-06-26 20:18:17 -0400

    contents test update

--- tests/testthat/test_wrap_contents.R
+++ tests/testthat/test_wrap_contents.R
@@ -5,8 +5,10 @@ wrap_commands(c("echo", shout="echo"),
               env = cenv)
 
 test_that("giftwrap contents are correct", {
-  expect_true(any(grepl(paste0("giftwrap\\('", "echo","',"), capture.output(cenv$echo))))
-  expect_true(any(grepl(paste0("giftwrap\\('", "echo","',"), capture.output(cenv$shout))))
-  expect_false(any(grepl(paste0("giftwrap\\('", "shout","',"), capture.output(cenv$echo))))
-  expect_false(any(grepl(paste0("giftwrap\\('", "shout","',"), capture.output(cenv$shout))))
+  expect_true(any(grepl("giftwrap",  capture.output(cenv$echo))))
+  expect_true(any(grepl("giftwrap", capture.output(cenv$shout))))
+  expect_true(any(grepl("echo",  capture.output(cenv$echo))))
+  expect_true(any(grepl("echo", capture.output(cenv$shout))))
+  expect_true(any(grepl(", ...",  capture.output(cenv$echo))))
+  expect_true(any(grepl(", ...", capture.output(cenv$shout))))
 })
