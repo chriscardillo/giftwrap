37c4bd23522f7966cb93b70833da65eed314fc8c
peppato <cfcardillo23@gmail.com>
2020-06-27 08:26:07 -0400

    test flags and options in wrap_commands

--- tests/testthat/test_wrap.R
+++ tests/testthat/test_wrap.R
@@ -1,22 +1,24 @@
 context("wrap_commands and wrap_lexicon generate commands")
 
+other_env <- new.env()
+
 wrap_commands("echo")
 wrap_commands("echo", use_namespace = "gifts")
-other_env <- new.env()
 wrap_commands("echo", env = other_env)
 
 wrap_lexicon(lexicon("git"),
              commands = "status")
-
 wrap_lexicon(lexicon("git"),
              commands = "status",
              env = other_env)
-
 wrap_lexicon(lexicon("aws"),
              commands = "s3$",
              subcommands = "ls$",
              use_namespace = "gifts",
              drop_base = T)
+
+wrap_commands("fake commmand -f --flag option",
+              env = other_env)
 
 test_that("wrap_commands creates commands", {
   expect_true(!is.null(echo))
@@ -36,3 +38,10 @@ test_that("wrap_lexicon drops base", {
 test_that("wrap_commands errors work", {
   expect_error(wrap_commands("echo", env = ""))
 })
+
+test_that("wrap_commands accepts flags in wrapping", {
+  expect_true(!is.null(other_env$fake_commmand_f_flag_option))
+  expect_true(any(grepl("fake commmand -f --flag option", 
+                        capture.output(other_env$fake_commmand_f_flag_option))))
+})
+
