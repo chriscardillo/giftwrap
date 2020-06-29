de9361b3ee9f61ef05b86df02bae8ba99a8f293a
peppato <cfcardillo23@gmail.com>
2020-06-29 08:57:12 -0400

    tests for temp_

---
+++ tests/testthat/test_temp.R
@@ -0,0 +1,11 @@
+context("temp_ functions write readable files")
+
+csv <- temp_csv(mtcars)
+rds <- temp_rds(mtcars)
+txt <- temp_lines(mtcars)
+
+test_that("files written with temp_ functions are loaded", {
+  expect_true(any(class(readr::read_csv(csv)) == "data.frame"))
+  expect_true(any(class(readr::read_rds(rds)) == "data.frame"))
+  expect_true(!is.null(readr::read_lines(txt)))
+})
